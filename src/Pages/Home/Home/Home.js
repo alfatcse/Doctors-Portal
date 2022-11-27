3cf0b4badb30a790f119e82ea217c5d73c28f6df
Alfat <alfat.cse.21@gmail.com>
2022-11-27 13:11:52 +0100

    Contact info done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import InfoCards from '../../InfoCards/InfoCards';
 
 import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div className='mx-5'>
             <Banner></Banner>
             <InfoCards></InfoCards>
+            <Services></Services>
         </div>
     );
 };
