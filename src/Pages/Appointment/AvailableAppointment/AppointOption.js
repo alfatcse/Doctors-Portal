e772b9f1f32a40261426152ad24bc445c54eeeb9
Alfat <alfat.cse.21@gmail.com>
2022-11-28 00:19:25 +0100

    card date send to modal

--- src/Pages/Appointment/AvailableAppointment/AppointOption.js
+++ src/Pages/Appointment/AvailableAppointment/AppointOption.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const AppointOption = ({ option }) => {
+const AppointOption = ({ option,setTreatment }) => {
     const { name, slots } = option;
-    return (
-        <div className="card  shadow-xl">
+    return ( 
+        <div className="card  shadow-xl ">
             <div className="card-body text-center">
                 <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
                 <p>{slots.length>0?slots[0]:'Try Another Day'}</p>
                 <p>{slots.length} {slots.length>1?'Spaces':'Space'} Available</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary text-white font-bold">Book Appointment</button>
+                    <label
+                     onClick={()=>setTreatment(option)}
+                    htmlFor="booking-modal" className="btn btn-primary text-white font-bold">Book Appointment</label>
                 </div>
             </div>
         </div>
--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import AppointOption from './AppointOption';
+import BookingModal from '../BookingModal/BookingModal';
 const AvailableAppointment = ({ selectedDate }) => {
-    const [appointmentOptions,setappointmentOptions]=useState([]);
-    useEffect(()=>{
+    const [appointmentOptions, setappointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+    useEffect(() => {
         fetch('appointmentOptions.json')
-        .then(res=>res.json())
-        .then(data=>setappointmentOptions(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setappointmentOptions(data))
+    }, [])
     return (
         <section className='mt-16'>
-            <p className='text-center text-primary font-bold'>date{format(selectedDate, 'PP')}</p>
-            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                 {
-                    appointmentOptions.map(option=><AppointOption key={option._id} option={option}></AppointOption>)
-                 }
+            <p className='text-center text-4xl text-primary font-bold'>Available Appointment On:{format(selectedDate, 'PP')}</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointmentOptions.map(option => <AppointOption key={option._id} option={option} setTreatment={setTreatment}></AppointOption>)
+                }
             </div>
+            {
+                treatment&&
+                <BookingModal treatment={treatment}></BookingModal>
+            }
         </section>
     );
 };
