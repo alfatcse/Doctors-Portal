e2ad1ce655fa8e5a7acb9cfb46704dda06ccfec7
Alfat <alfat.cse.21@gmail.com>
2022-11-27 16:58:07 +0100

    Global button implemented

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import doctor from '../../../assets/images/doctor.png';
+import appointment from '../../../assets/images/appointment.png';
+import PrimaryButton from '../../Components/PrimaryButton/PrimaryButton';
 const MakeAppointment = () => {
     return (
-        <section>
-            <div className="hero min-h-screen bg-base-200">
+        <section className='mt-32 rounded' style={{ background:`url(${appointment})`   }}>
+            <div className="hero ">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src={doctor} alt className="lg:w-1/2 -mt-32 -mb-4 hidden md:block rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h4 className='text-xl font-bold text-primary'>Appointment</h4>
+                        <h1 className="text-4xl text-white font-bold">Make an appointment Today</h1>
+                        <p className="py-6 text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
+                        <PrimaryButton>Make Appointment</PrimaryButton>
                     </div>
                 </div>
             </div>
