24892fe166a06200cabfb9ea2a472c02f3c66786
Alfat <alfat.cse.21@gmail.com>
2022-11-30 22:40:58 +0100

    image uploaded to image hosting site done

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import {useQuery} from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { async } from '@firebase/util';
 import Loading from '../../Shared/Loading/Loading';
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const {data:specialties,isLoading}=useQuery({
-        queryKey:['specialty'],
-        queryFn:async()=>{
-            const res=await fetch('http://localhost:5006/appointmentSpecialty');
-            const data=await res.json();
+    const imageHostKey = process.env.REACT_APP_imgbb_key;
+    const formData = new FormData();
+    const { data: specialties, isLoading } = useQuery({
+        queryKey: ['specialty'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5006/appointmentSpecialty');
+            const data = await res.json();
             console.log(data);
             return data;
         }
     })
     const handleAddDoctor = (data) => {
-        console.log(data);
+        console.log(data.image[0]);
+        const image = data.image[0];
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageHostKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+                    console.log(imgData.data.url);
+                    const doctor={
+                        name:data.name,
+                        email:data.email,
+                        specialty:data.specialty,
+                        image:imgData.data.url
+                    }
+                }
+            })
     }
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
     return (
@@ -42,9 +63,9 @@ const AddDoctor = () => {
                     <label className="label">
                         <span className="label-text">Please Select a Specialty</span>
                     </label>
-                    <select {...register("specialty", { required: "Specialty is Required" })}  className="select input-bordered w-full max-w-xs">
+                    <select {...register("specialty", { required: "Specialty is Required" })} className="select input-bordered w-full max-w-xs">
                         {
-                           specialties.map(specialty=><option value={specialty.name} key={specialty._id}>{specialty.name}</option>)
+                            specialties.map(specialty => <option value={specialty.name} key={specialty._id}>{specialty.name}</option>)
                         }
                         <option>Svelte</option>
                     </select>
@@ -53,7 +74,7 @@ const AddDoctor = () => {
                     <label className="label">
                         <span className="label-text">Photo</span>
                     </label>
-                    <input type="file" {...register("img", { required: "Name is required" })} className="input input-bordered w-full max-w-xs" />
+                    <input type="file" {...register("image", { required: "Name is required" })} className="input input-bordered w-full max-w-xs" />
                     {errors.img && <p className='text-red-600'>{errors.img.message}</p>}
                 </div>
                 <input className='btn mt-5 w-full btn-accent' value="Add Doctor" type="submit" />
