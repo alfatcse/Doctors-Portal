46754655ba03fdd69709a0ead99211c5eb825607
Alfat <alfat.cse.21@gmail.com>
2022-12-16 20:26:50 +0100

    multiple date slot done

--- src/Pages/Dashboard/DoctorDash/MakeSchedule.js
+++ src/Pages/Dashboard/DoctorDash/MakeSchedule.js
@@ -5,21 +5,13 @@ import { addDays, format } from 'date-fns';
 import backImg from '../../../assets/images/bg.png'
 import { useState } from 'react';
 const MakeSchedule = () => {
-    const d = new Date();
     var option = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const initialDays = d;
-    // console.log('in', initialDays);
-    const d1 = new Date();
     const [days, setDays] = useState();
     const [daySlot, setdaySlot] = useState([{
         date: '',
-        slot:''
+        slot: []
     }])
-    // if (days && days.length > 0) {
-    //     days.map(d => {
-    //         console.log('ddd::', d);
-    //     })
-    // }
+    const [slot,setSlot]=useState([]);
     const footer =
         days && days.length > 0 ? (
             <p>You selected {days.length} day(s).</p>
@@ -27,19 +19,35 @@ const MakeSchedule = () => {
             <p>Please pick one or more days.</p>
         );
     const handleSlot = (date) => {
-        //console.log(date)
-        const c=[];
-        date.map(d=>{
-            const a={
-                date:d,
-                slot:'10-12'
+
+        const c = [];
+        date.map(d => {
+            const a = {
+                date: `${d.toLocaleDateString('en-US', option)}`,
+               // slot: ['2', '3', '4']
             }
             c.push(a);
         })
         //console.log(c);
         setdaySlot(c);
     }
-    console.log(daySlot)
+   // console.log(daySlot);
+    const handleDaySlot = d=>(event) => {
+        
+        event.preventDefault();
+        const s=[event.target.slot1.value,event.target.slot2.value];
+        
+        setSlot(s);
+        d.slot=slot;
+        console.log('slott', d); 
+    }
+   // console.log(slot)
+    const handelSlots = (d) => {
+       
+        d.slot=slot;
+        console.log('slott', d);
+        setSlot(null);
+    }
     return (
         <section>
             <header className='my-6' style={{ backgroundImage: `url(${backImg})` }}>
@@ -58,18 +66,26 @@ const MakeSchedule = () => {
                         </div>
                     </div>
                 </div>
-               
             </header>
-            <label onClick={()=>handleSlot(days)} htmlFor='slot-modal' className="btn ml-20 btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary">Add Slot</label>
-            {
+            <label onClick={() => handleSlot(days)} htmlFor='slot-modal' className="btn ml-20 btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-primary">Add Slot</label>
+            {daySlot &&
                 <>
                     <input type="checkbox" id="slot-modal" className="modal-toggle" />
                     <div className="modal">
                         <div className="modal-box">
-                            <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
-                            {/* {
-                                days.map(d => console.log('date' ,d))
-                            } */}
+                            {
+                                daySlot.map(d =>
+                                    <>
+                                        <p>{d.date}</p>
+                                        <form onSubmit={handleDaySlot(d)}>
+                                            <input placeholder='slot' name='slot1' type="text" />
+                                            <input placeholder='slot' name='slot2' type="text" />
+                                            <button onClick={e=>e.currentTarget.disabled=true} className='btn btn-primary'  value="Log In" type="submit" >Add</button>
+                                        </form>
+                                    </>
+                                )
+
+                            }
                             <div className="modal-action">
                                 <label htmlFor="slot-modal" className="btn">Yay!</label>
                             </div>
