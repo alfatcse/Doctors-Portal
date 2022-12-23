316c84255c8bb2fac065a187b8a77cbf9e102ac4
Alfat <alfat.cse.21@gmail.com>
2022-12-24 00:47:35 +0100

    schedule date loaded

--- src/Pages/Dashboard/DoctorDash/MySchedule.js
+++ src/Pages/Dashboard/DoctorDash/MySchedule.js
@@ -1,35 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Context/AuthProvider";
 
 const MySchedule = () => {
-
-    return (
-         <div>
+  const { user } = useContext(AuthContext);
+  axios
+    .get(`http://localhost:5006/myschedule?email=${user.email}`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+    .then((res) => {
       
-          <h1 className="text-center text-2xl font-bold mb-3 text-blue-600">
-            My Schedule
-          </h1>
-          <div className="overflow-x-auto">
-            <table className="table w-full">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Date</th>
-                  <th>Payment Status</th>
-                  <th>Slot</th>
-                  <th>Call</th>
-                </tr>
-              </thead>
-              <tbody>
-               
-              </tbody>
-            </table>
-          </div>
-      
+    })
+    .catch((e) => console.log(e));
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold mb-3 text-blue-600">
+        My Schedule
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Date</th>
+              <th>Payment Status</th>
+              <th>Slot</th>
+              <th>Call</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
-    );
+  );
 };
 
 export default MySchedule;
