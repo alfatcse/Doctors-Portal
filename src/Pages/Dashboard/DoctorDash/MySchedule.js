0f02606c991d2e0e10aa2ed04dc37769ec58d16c
Alfat <alfat.cse.21@gmail.com>
2022-12-24 01:16:34 +0100

    schedule date displayed

--- src/Pages/Dashboard/DoctorDash/MySchedule.js
+++ src/Pages/Dashboard/DoctorDash/MySchedule.js
@@ -1,22 +1,28 @@
 import axios from "axios";
-import React, { useContext } from "react";
+import { th } from "date-fns/locale";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Context/AuthProvider";
 
 const MySchedule = () => {
   const { user } = useContext(AuthContext);
-  axios
-    .get(`http://localhost:5006/myschedule?email=${user.email}`, {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(),
-    })
-    .then((res) => {
-      
-    })
-    .catch((e) => console.log(e));
+  const [schedule, setSchedule] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5006/myschedule?email=${user.email}`, {
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(),
+      })
+      .then((res) => {
+        console.log(res.data.docSlot);
+        setSchedule(res.data.docSlot);
+      })
+      .catch((e) => console.log(e));
+  }, [user.email]);
+  console.log("sss", schedule);
   return (
     <div>
       <h1 className="text-center text-2xl font-bold mb-3 text-blue-600">
@@ -27,15 +33,21 @@ const MySchedule = () => {
           <thead>
             <tr>
               <th></th>
-              <th>Name</th>
-              <th>Email</th>
               <th>Date</th>
-              <th>Payment Status</th>
               <th>Slot</th>
-              <th>Call</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {schedule.map((d, i) => (
+              <tr>
+                <th>{i + 1}</th>
+                <td>{d.date}</td>
+                <td className="mx-2">{
+                  d.slot
+                  }</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
